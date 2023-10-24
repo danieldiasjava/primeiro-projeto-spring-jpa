72fef955b4062144dc87681d1690a0722a11a574
Daniel Dias <danieldias.devjava@gmail.com>
2023-10-24 10:34:26 -0300

    Payment, one-to-one association

--- src/main/java/com/primeiroprojetospring/course/config/TesteConfig.java
+++ src/main/java/com/primeiroprojetospring/course/config/TesteConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.primeiroprojetospring.course.entities.Category;
 import com.primeiroprojetospring.course.entities.Order;
 import com.primeiroprojetospring.course.entities.OrderItem;
+import com.primeiroprojetospring.course.entities.Payment;
 import com.primeiroprojetospring.course.entities.Product;
 import com.primeiroprojetospring.course.entities.User;
 import com.primeiroprojetospring.course.entities.enums.OrderStatus;
@@ -85,7 +86,11 @@ public class TesteConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1 ); 
+		o1.setPayment(pay1);
 
+		
+		orderRepository.save(o1);
 	}
 
 }
